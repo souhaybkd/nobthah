267f607dd1c3f63e71df87a8d70df7e81ae76166
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-06-04 12:10:27 +0530

    Added register page

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,13 +1,30 @@
 import React from "react"
 import "./register.scss"
-import Navbar from '../../components/Navbar/Navbar'
-import Form from "./components/Form"
-import Content from "./components/Content"
 
 export default function Register() {
   return (
     <main className="register-page">
-        <Form></Form>
+      <div className="form">
+        <div className="left">
+          x
+        </div>
+        <div className="right">
+          <h2>Welcome To Nobthah</h2>
+          <p>Create beautiful resumes at speed</p>
+          <input className="name-input" type="text" placeholder="Enter Your Name" />
+          <input type="text" placeholder="Enter Your email" />
+          <input type="text" placeholder="Create A New Password" />
+          <button>Create A New Account</button>
+          <div className="or">
+            <div className="line"></div>
+            <span>OR</span>
+            <div className="line"></div>
+          </div>
+          <button>Visit Login Page</button>
+          <button>Forgot Your Password</button>
+        </div>
+      </div>
+      <div className="overlay"></div>
     </main>
   )
 }
--- src/pages/register/components/Form.jsx
+++
@@ -1,9 +0,0 @@
-import "./form.scss"
-
-export default function Form() {
-  return (
-    <div className="form">
-        <h2>Welcome To Nobthah</h2>
-    </div>
-  )
-}
